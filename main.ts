1fd8fef291ff4e47705dbb7aac1db1f1a17441a0
Maria-Varghese <maria.varghese@mths.ca>
2024-09-17 11:09:12 -0400

    did maths

--- main.ts
+++ main.ts
@@ -2,7 +2,11 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program does basic math
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString('p=2*(5+3) = '+ (2 * (5 + 3)).toString())
+basic.showString('a = 5*3 = ' + (5*3).toString())
